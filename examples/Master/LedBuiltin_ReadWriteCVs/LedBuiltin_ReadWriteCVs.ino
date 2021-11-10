b11e933228290e43c16b6a7c498f2a7e8c8940cb
TheFidax <58255296+TheFidax@users.noreply.github.com>
2021-11-10 14:17:31 +0100

    Update LedBuiltin_ReadWriteCVs.ino

--- examples/Master/LedBuiltin_ReadWriteCVs/LedBuiltin_ReadWriteCVs.ino
+++ examples/Master/LedBuiltin_ReadWriteCVs/LedBuiltin_ReadWriteCVs.ino
@@ -4,15 +4,18 @@
 *   -   Permette la lettura/scrittura delle CVs salvate nella EEPROM del Microcontrollore
 */
 
-#include <stdint.h>     // Libreria per i tipi "uintX_t"
-#include <Rcn600.h>     // Includo la libreria per la gestione della SUSI
-#include <EEPROM.h>     // Libreria per la gestione della EEPROM interna
-#include <SusiOverI2c.h>
+#include <stdint.h>             // Libreria per i tipi "uintX_t"
+#include <Rcn600.h>             // Includo la libreria per la gestione della SUSI
+#include <EEPROM.h>             // Libreria per la gestione della EEPROM interna
+#include <WireSusi.h>           // Libreria per la gestione dei messaggi Rcn600 Susi tramite Wire / I2c
 
-Rcn600 SUSI(2, 3);      // (CLK pin, DATA pin) il pin di Clock DEVE ESSERE di tipo interrupt, il pin Data puo' essere in pin qualsiasi: compresi gli analogici
+#define MASTER_ADDRESS  0       // Indirizzo del Master del bus I2c / wire
+#define SLAVE_ADDRESS   1       // Indirizzo dello Slave
 
-void notifySusiRawMessage(uint8_t firstByte, uint8_t secondByte) {
-    sendCommandSusiOverI2c(1, firstByte, secondByte);
+Rcn600 SUSI(2, 3);              // (CLK pin, DATA pin) il pin di Clock DEVE ESSERE di tipo interrupt, il pin Data puo' essere in pin qualsiasi: compresi gli analogici
+
+void notifySusiRawMessage(uint8_t firstByte, uint8_t secondByte) {                                                  // Funzione CallBack che viene invocata quando e' disponibile un messaggio Susi
+    sendCommandWireSusi(SLAVE_ADDRESS, firstByte, secondByte);                                                      // Invio il comando Susi ricevuto allo Slave
 }
 
 void notifySusiFunc(SUSI_FN_GROUP SUSI_FuncGrp, uint8_t SUSI_FuncState) {                                           // Funzione CallBack che viene invocata quando e' decodificato un comando per le Funzioni
@@ -26,32 +29,32 @@ void notifySusiFunc(SUSI_FN_GROUP SUSI_FuncGrp, uint8_t SUSI_FuncState) {       
 }
 
 uint8_t notifySusiCVRead(uint16_t CV) {                                                                             // Funzione CallBack per leggere il valore di una CV memorizzata
-    if(CV == 910) {
-        return readCVsSusiOverI2c(1, CV);
+    if(CV == 910) {                                                                                                 // Se la CV da leggere e' la 910
+        return readCVsWireSusi(SLAVE_ADDRESS, CV);                                                                  // Allora comunico allo Slave I2c di Leggere il valore della CV dal proprio sistema di memorizzazione
     }
     else {
-        return EEPROM.read(CV);                                                                                         // Restituisce il valore memorizzato nella EEPROM
+        return EEPROM.read(CV);                                                                                     // Restituisce il valore memorizzato nella EEPROM
     }
 }
 
 uint8_t notifySusiCVWrite(uint16_t CV, uint8_t Value) {                                                             // Funzione CallBack per scrivere il valore di una CV memorizzata
-    if(CV == 910) {
-        return writeCVsSusiOverI2c(1, CV, Value);
+    if(CV == 910) {                                                                                                 // Se la CV da leggere e' la 910
+        return writeCVsWireSusi(SLAVE_ADDRESS, CV, Value);                                                          // Allora comunico allo Slave I2c di Scrivere il valore della CV nel proprio sistema di memorizzazione
     }
     else {
-        EEPROM.update(CV, Value);                                                                                       // Se il valore richiesto e' diverso aggiorno la EEPROM, in caso contrario non modifico per non rovinarla
+        EEPROM.update(CV, Value);                                                                                   // Se il valore richiesto e' diverso aggiorno la EEPROM, in caso contrario non modifico per non rovinarla
 
-        return EEPROM.read(CV);                                                                                         // Restituisco il nuovo valore della EEPROM
+        return EEPROM.read(CV);                                                                                     // Restituisco il nuovo valore della EEPROM
     }
 }
 
 void setup() {                                                                                                      // Setup del Codice
     pinMode(LED_BUILTIN, OUTPUT);                                                                                   // Imposto il pin a cui e' connesso il LED_BUILTIN come output
-    Wire.begin(0);
-    Wire.setClock(400000);
-    SUSI.init();                                                                                                    // Avvio la libreria
+    Wire.begin(MASTER_ADDRESS);                                                                                     // Imposto la libreria Wire come Master del bus con l'indirizzo specificato
+    Wire.setClock(400000);                                                                                          // Imposto la frequenza del bus I2c a 400kHz (Fast I2c)
+    SUSI.init();                                                                                                    // Avvio la libreria Rcn600 per l'acquisizione del Segnale SUSI
 }
 
 void loop() {                                                                                                       // Loop del codice
-    SUSI.process();                                                                                                 // Elaboro piu' volte possibile i dati acquisiti dalla libreria
+    SUSI.process();                                                                                                 // Elaboro piu' volte possibile i dati acquisiti dalla libreria Rcn600 Susi
 }
